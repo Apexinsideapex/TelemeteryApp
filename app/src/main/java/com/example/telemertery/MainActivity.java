acb49430b828951cbd75fedabda7ed81b1550f27
Apexinsideapex <r49.sharma@gmail.com>
2021-02-03 01:43:48 +0530

    Made some small changes

--- app/src/main/java/com/example/telemertery/MainActivity.java
+++ app/src/main/java/com/example/telemertery/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.telemertery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button go;
+    TextView GPSinfo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        go = findViewById(R.id.button_view_go);
+        GPSinfo = findViewById(R.id.text_view_gps);
+
+        go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+                int latitude = (int) (location.getLatitude());
+                GPSinfo.setText(latitude);
+            }
+        });
+        
     }
 }
